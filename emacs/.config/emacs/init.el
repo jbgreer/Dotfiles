c954d997341c5c81bbba9b7c57b1e7ee321dd445
Jim Greer <jbgreer@gmail.com>
2023-06-19 14:55:34 -0500

    initial emacs config

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,9 @@
+;; 2023-06-18 jbgreer
+
+(setq inhibit-startup-message t)    ; Disable startup splash screen
+
+(scroll-bar-mode -1)                ; Disable visible scrollbar
+(tool-bar-mode -1)                  ; Disable toolbar
+(tooltip-mode -1)                   ; Disable tooltips
+(set-fringe-mode 10)
+
