ba8bb8f939d90b40efaf91f423d76d94e5568041
Jim Greer <jbgreer@gmail.com>
2023-06-20 17:50:02 -0500

    updated emacs configs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,5 +1,4 @@
-;; 2023-06-18 jbgreer
-
+; 2023-06-18 jbgreer
 (setq inhibit-startup-message t)    ; Disable startup splash screen
 
 (scroll-bar-mode -1)                ; Disable visible scrollbar
@@ -7,3 +6,11 @@
 (tooltip-mode -1)                   ; Disable tooltips
 (set-fringe-mode 10)
 
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
