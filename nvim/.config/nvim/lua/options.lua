e2a4f5a80dbc2422f48867899621ad6e37556094
Jim Greer <jbgreer@gmail.com>
2024-05-31 16:45:08 -0500

    moving away from LazyVim

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,36 @@
+vim.opt.guicursor = ""
+vim.opt.termguicolors = true
+
+-- leader
+vim.g.mapleader = ' '
+
+-- sensible indention defaults
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- no line-wrapping
+vim.opt.wrap = false
+
+-- no swapfile or backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- no highlighting on search, but incremental search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- line numbers
+vim.opt.number = true
+vim.opt.numberwidth = 4
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
+-- turn off language integrations
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+
