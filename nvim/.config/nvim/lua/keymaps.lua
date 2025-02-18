4fe727a9d795ef5b93565bf9b10d773f3e69b571
Jim Greer <jbgreer@gmail.com>
2025-02-18 16:59:02 -0600

    lazyvim

--- nvim/.config/nvim/lua/keymaps.lua
+++
@@ -1,6 +0,0 @@
-
--- vim pane navigation
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
